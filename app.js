225b4f7a6affb253b7c6678b3a8cbb67fecf523c
ashu00026 <19341a1207@gmrit.edu.in>
2023-05-03 21:58:00 +0530

    allow origin

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 // const express = require('express');
 // const app=express();
 // require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const cors = require("cors");
+const corsOptions = {
+  // origin:'http://localhost:3000',
+  origin: "https://visualizeapp.onrender.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
-
-const connectDB = require('./db/connectDb');
-const Router = require('./routes/app');
+const connectDB = require("./db/connectDb");
+const Router = require("./routes/app");
 
 // const notFoundMiddleware = require('./middleware/not-found');
 // const errorMiddleware = require('./middleware/error-handler');
@@ -26,17 +24,17 @@ app.use(express.json());
 
 // routes
 
-app.use('/home',Router);
-
-
+app.use("/home", Router);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     // connectDB
-    
-    await connectDB('mongodb+srv://ashutosh:Ashu5656@nodeexpressprojets.nuuengz.mongodb.net/Task?retryWrites=true&w=majority');
+
+    await connectDB(
+      "mongodb+srv://ashutosh:Ashu5656@nodeexpressprojets.nuuengz.mongodb.net/Task?retryWrites=true&w=majority"
+    );
     app.listen(port, () => console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
